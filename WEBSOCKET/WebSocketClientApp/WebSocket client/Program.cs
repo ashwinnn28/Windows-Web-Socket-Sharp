143af853a036299be47065c646417373c33bc21d
Ashwin <ashwinnn28@yahoo.com>
2024-01-08 17:53:56 +0530

    Add files via upload

---
+++ WEBSOCKET/WebSocketClientApp/WebSocket client/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using WebSocketSharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var ws = new WebSocket("ws://localhost:8080/chat"))
+        {
+            ws.OnOpen += (sender, e) =>
+            {
+                Console.WriteLine("WebSocket connection opened.");
+            };
+
+            ws.OnMessage += (sender, e) =>
+            {
+                Console.WriteLine($"Received Message: {e.Data}");
+            };
+
+            ws.OnError += (sender, e) =>
+            {
+                Console.WriteLine($"WebSocket error: {e.Message}");
+            };
+
+            ws.OnClose += (sender, e) =>
+            {
+                Console.WriteLine($"WebSocket connection closed. Code: {e.Code}, Reason: {e.Reason}");
+            };
+
+            ws.Connect();
+            while (true)
+            {
+                ws.Send("Hello, Server!"); 
+                Console.ReadLine();
+
+            }
+            Console.WriteLine("WebSocket Client connected to ws://localhost:8080/chat");
+
+
+            
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ WEBSOCKET/WebSocketServerApp/WebSocketServerApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+class MyWebSocketService : WebSocketBehavior
+{
+    protected override void OnOpen()
+    {
+        Console.WriteLine($"WebSocket connection opened. ID: {ID}");
+    }
+
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        string receivedMessage = e.Data;
+        Console.WriteLine($"Received Message: {receivedMessage}");
+
+        // Send a response back to the client
+        Send("Server received your message: " + receivedMessage);
+    }
+
+    protected override void OnError(ErrorEventArgs e)
+    {
+        Console.WriteLine($"WebSocket error: {e.Message}");
+    }
+
+    protected override void OnClose(CloseEventArgs e)
+    {
+        Console.WriteLine($"WebSocket connection closed. ID: {ID}, Code: {e.Code}, Reason: {e.Reason}");
+    }
+}
+
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var wssv = new WebSocketServer(8080);
+        wssv.AddWebSocketService<MyWebSocketService>("/chat");
+        wssv.Start();
+
+        Console.WriteLine("WebSocket Server is running on ws://localhost:8080/chat");
+        Console.ReadLine();
+
+        wssv.Stop();
+    }
+}
+
